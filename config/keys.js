92e870f2d5fc88e0e78a80d3592309bb09ee6537
Andrea Riccetti <RICCETTI@sk.ibm.com>
2018-02-28 23:28:53 +0100

    asdsa asdsa

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
-module.exports = {
-  googleClientId: '154342261728-lk55ah8k4u5vr8n0b2scvrvvnlknn4lp.apps.googleusercontent.com',
-  googleSecretId: '4wyx9P30g3VUJpUX_2EqPfAv',
-  mongoUri: 'mongodb://andreagulp:andric09856390@ds151348.mlab.com:51348/emaily-dev-002',
-  cookieKey: 'asdasdaswqewqewq32i42384798yrhdssbdf7q3yu'
+if (process.env.NODE_ENV === 'production') {
+  //prod
+  module.exports = require('./prod')
+} else {
+  //dev
+  module.exports = require('./dev')
 }
