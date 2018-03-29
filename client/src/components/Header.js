8eb4d399f3be8cf91a53f2ba72dcd8d28f9c9d03
andreagulp <andreacommunication@gmail.com>
2018-03-30 00:55:44 +0200

    4

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 
 import LogIn from './LogIn';
 
 class Header extends Component {
 
-  render () {
+  render() {
     return (
       <AppBar
         title="eLiquid Lab"
         onLeftIconButtonTouchTap={this.props.handleToggle}
-        iconElementRight={<LogIn user={this.props.user}/>}
+        iconElementRight={<LogIn user={this.props.user} />}
       />
     )
   }
 };
 
-const mapStateToProps = (state) => { return {user: state.user}}
+const mapStateToProps = (state) => { return { user: state.user } }
 
 export default connect(mapStateToProps, null)(Header);
