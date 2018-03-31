816eddc85ee8c27097ef8c0397227a14b99366a9
andreagulp <andreacommunication@gmail.com>
2018-04-01 01:07:56 +0200

    2

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise'
-import thunk from 'redux-thunk';
+import reduxThunk from 'redux-thunk';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import reducers from './reducers';
 import './index.css';
 import App from './App';
 
-const createStoreWithMiddleware = applyMiddleware (promiseMiddleware, thunk) (createStore)
+// const createStoreWithMiddleware = applyMiddleware (promiseMiddleware, thunk) (createStore)
+const store = createStore(reducers, {}, applyMiddleware(promiseMiddleware, reduxThunk));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={store}>
+    {/* <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}> */}
     <BrowserRouter>
       <MuiThemeProvider>
         <App />
