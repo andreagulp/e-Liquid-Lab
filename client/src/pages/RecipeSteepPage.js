31cab91a896ba990e73f138e36fb663c4dee04a1
andreagulp <andreacommunication@gmail.com>
2018-06-25 15:35:35 +0200

    v2.3

---
+++ client/src/pages/RecipeSteepPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class RecipeSteepPage extends Component {
+  render() {
+    return (
+      <div>
+        <h4>RecipeSteepPage Component Working</h4>
+      </div>
+    );
+  }
+}
+
+export default RecipeSteepPage;
